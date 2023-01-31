6d12a7bbb88b4d5dd6d6e323cea9b07b3561b4de
Raymond An <raymond_an@yahoo.com>
2023-01-31 02:30:13 -0500

    commit tree cd789abcf70e62f9778b3b1f3fdfc75968d22c1c

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove()
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.innerHTML = "<h1 id='victory'>Noodles is the champion</h1>";
+document.body.append(newHeader);
